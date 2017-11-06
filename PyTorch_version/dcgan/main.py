936144c8548055dfd7b369cc1d7a3c6738986011
rex <rexwang@bu.edu>
2017-11-06 00:06:40 -0500

    Add Dcgan Pytorch implementation.

---
+++ PyTorch_version/dcgan/main.py
@@ -0,0 +1,14 @@
+from __future__ import print_function  # Py 2/3 Compatible
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
---
+++ PyTorch_version/dcgan/model.py
@@ -0,0 +1,96 @@
+from __future__ import print_function  # Py 2/3 Compatible
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+
+
+ngpu = None
+nz = None
+ngf = None
+ndf = None
+nc = 3
+
+# Custom weights initialization called on G and D
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+class _G(nn.Module):
+    def __init__(self, ngpu):
+        super(_G, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input Z is going to a convolution
+            nn.ConvTranspose2d(in_channels=nz,
+                               out_channels=ngf * 8,
+                               kernel_size=4,
+                               stride=1,
+                               padding=0,
+                               bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(in_channels=ngf * 8,
+                               out_channels=ngf * 4,
+                               kernel_size=4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(in_channels=ngf * 4,
+                               out_channels=ngf * 2,
+                               kernel_size=4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(in_channels=ngf * 2,
+                               out_channels=ngf,
+                               kernel_size=4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(in_channels=ngf,
+                               out_channels=nc,
+                               kernel_size=4,
+                               stride=2,
+                               padding=1,
+                               bias=False),
+            nn.Tanh()
+
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        else:
+            output = self.main(input)
+        return output
+
